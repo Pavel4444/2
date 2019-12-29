054459b3d16858690531016916ed6960a1bacf61
Pavel4444 <qwert79@seznam.cz>
2019-12-29 17:59:34 +0100

    Updates

---
+++ precache-manifest.2931259ea011d528a05c533aa202ad8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd75b7cc303caa32d3ad8f3993dbe17",
+    "url": "/2/index.html"
+  },
+  {
+    "revision": "e0379890fcb130bce4d5",
+    "url": "/2/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "706f41ffbdf26bf106bf",
+    "url": "/2/static/js/2.39513104.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/2/static/js/2.39513104.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e0379890fcb130bce4d5",
+    "url": "/2/static/js/main.2c1fd362.chunk.js"
+  },
+  {
+    "revision": "6379e9521a82c9e4ba79",
+    "url": "/2/static/js/runtime-main.67ccd6d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/2/static/media/logo.5d5d9eef.svg"
+  }
+]);
